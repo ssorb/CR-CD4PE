3235eb4138d85cabacdc02ac6d811b62ae7d00ea
Administrator <admin@example.com>
2020-09-03 13:18:30 +0000

    Add new file

---
+++ site-modules/profile/manifests/platform/se_secteam_cis/standard_baseline.pp
@@ -0,0 +1,20 @@
+# @summary Entry point for Security Team's CIS module.
+#
+# This module enforces CIS benchmark settings for Windows 2019 and Centos 8 servers. 
+# There is a single parameter to optionally set noop mode for the scope of this class.
+#
+# @example
+#   include secteam::standard_baseline
+class secteam_cis::standard_baseline {
+
+  $fam = $facts.get('os.family').downcase()
+  $maj = $facts.get('os.release.major').downcase()
+
+  if defined("secteam_cis::${fam}::${fam}_${maj}") {
+    include "secteam_cis::${fam}::${fam}_${maj}"
+  }
+  else {
+    fail('secteam_cis - unsupported operating system, please see readme')
+  }
+
+}
